721eaecd31b53b4f14099250b42dbf2d6a182d3b
edwardbryant <edward.bryant@gmail.com>
2015-02-14 17:24:08 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 
-var apiKey = "fed1050696146f0081c5285f5fd827c5";
-
 $(document).ready(function() {
     
     updateDate();
-    pulsate('#ticket');
+    pulse('#ticket');
+    
 
-    getMovieId("Idiocracy");
+    getMovieId("star trek");
 
+    getScore();
 
 
 
 });
 
-function updateDate() {
+
+var updateDate = function() {
     var d = new Date();
     var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
     var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
@@ -23,26 +24,34 @@ function updateDate() {
     $("#day").html("&bull;" + day + "&bull;"); 
     $("#month").text(month); 
     $("#date").text(date);
-}
-
-function cleanText(text) {
-
-    // var text = " A movie, which is a film, about a fun-loving man's adventure in time. You will love it!";
-
-    // replace hyphen with whitespace, before punc stripped 
+};
+var pulse = function(element) {
+    // lower opacity 
+    var fadeItIn = function() {
+        $(element).fadeTo('600', 0.6, function () {
+            fadeItOut();
+        });
+    }
+    // increase opacity 
+    var fadeItOut = function() {
+        $(element).fadeTo('600', 1.0, function () {
+            fadeItIn();
+        });
+    }
+    // fadeIn if hidden, then start opacity cycling   
+    $(element).fadeIn('slow', function() {
+        fadeItOut();
+    });
+};
+var cleanText = function(text) {
+    text = text.replace(/ -- /g,' ');
     text = text.replace(/ - /g,' ');
     text = text.replace(/-/g,' ');
-
-    // remove remaining punc and leave no space
     text = text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-
-    // trim any whitespace from start and end of str  
+    text = text.toLowerCase();
     text = text.trim();
-
     return text;
-
-}
-
+};
 var getMovieId = function(q) {
     console.log("API called to find movie id");
     var request = {
@@ -64,7 +73,6 @@ var getMovieId = function(q) {
         getMovieDetails(id);
     });
 };
-
 var getMovieDetails = function(id){
     console.log("API movie detail call on ID " + id);
     var request = {
@@ -100,32 +108,25 @@ var getMovieDetails = function(id){
     });
 };
 
+var getScore = function(text) {
+
+
+    var cKeywords = ["beautiful","beauty","breakup","boyfriend","bride","bridesmaid","bridesmaids","couple","couples","cry","dance","dancing","date","diamond","diamonds","diary","diaries","divorce","divorced","dream","dress","dresses","fashion","friend","friends","girlfriend","girlfriends","heart","irresistible","kiss","kissing","love","marriage","paris","piano","pink","relationship","romance","romantic","sad","saddest","sister","sisterhood","spa","social","sweet","unfaithful","vows","wedding","weddings"];
+
+    var gKeywords = ["alien","apocalypse","apocalyptic","armor","army","athelete","atheletes","battle","baseball","blood","bloody","boxer","boxing","coach","coaching","corrupt","corruption","cop","cowboy","crime","crimes","criminal","cyborg","cyborgs","dead","death","die","disaster","destroy","destroys","destructive","dragon","dragons","drug","drugs","fbi","fight","fighter","fighting","football","gang","gangster","gangsters","gladiator","gladiators","golf","gun","guns","hockey","hostage","hostages","hunt","hunting","invade","invasion","jail","kill","killer","killing","maniac","marines","martial","maverick","military","mob","mobster","murder","navy","outlaw","outlaws","pentagon","platoon","police","prison","prisoners","psychopath","revenge","robbery","samurai","security","serial","shield","slaughter","soldier","soldiers","superhero","sword","syndicate","terror","terrorist","terrorists","victim","vikings","vietnam","violence","violent","war","warrior","warriors","world","zombie","zombies"];
+
+    console.log("C Keywords ... " + cKeywords.length);
+    console.log("G Keywords ... " + gKeywords.length);
+
+    var score = 1;
+    return score;
+}
+
+
+// testing stuff
+
 var testR = function(details){
     console.log(details['overview_raw']);
     console.log(details['overview']);
 };
 
-function chicScore(text) {
-    var chicWords = ["beautiful","beauty","breakup","boyfriend","bride","bridesmaid","bridesmaids","couple","couples","cry","dance","dancing","date","diamond","diamonds","diary","diaries","divorce","divorced","dream","fashion","friend","friends","girlfriend","girlfriends","heart","irresistible","kiss","kissing","love","marriage","paris","piano","pink","relationship","romance","romantic","sad","saddest","sister","sisterhood","spa","social","sweet","unfaithful","vows","wedding","weddings"];
-    var score = 1;
-    return score;
-}
-
-function pulsate(element) {
-    // lower opacity 
-    var fadeItIn = function() {
-        $(element).fadeTo('600', 0.6, function () {
-            fadeItOut();
-        });
-    }
-    // increase opacity 
-    var fadeItOut = function() {
-        $(element).fadeTo('600', 1.0, function () {
-            fadeItIn();
-        });
-    }
-    // fadeIn if hidden, then start opacity cycling   
-    $(element).fadeIn('slow', function() {
-        fadeItOut();
-    });
-}
